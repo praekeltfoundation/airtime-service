5ea7df2ea69273c2bc3a9e6cdbf534625dccc06d
Jeremy Thurgood <firxen@gmail.com>
2013-10-07 17:22:21 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="airtime-service",
+    version="0.1.0",
+    url='https://github.com/praekelt/airtime-service',
+    license='MIT',
+    description="A RESTful service for issuing airtime.",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.org',
+    packages=["airtime_service"],
+    install_requires=["Twisted", "klein", "sqlalchemy", "alchimia>=0.4"],
+)
